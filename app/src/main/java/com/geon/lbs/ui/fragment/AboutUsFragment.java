886a9682e6ae74f8066c353afece452db79b6a9c
babu <to.babu.khan@gmail.com>
2018-10-22 02:47:44 +0600

    almost done

--- app/src/main/java/com/geon/lbs/ui/fragment/AboutUsFragment.java
+++ app/src/main/java/com/geon/lbs/ui/fragment/AboutUsFragment.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,6 +22,18 @@ public class AboutUsFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
        mView = inflater.inflate(R.layout.fragment_about_us,container,false);
+        Toolbar toolbar = (Toolbar)mView.findViewById(R.id.app_bar);
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        activity.setSupportActionBar(toolbar);
+        activity.getSupportActionBar().setTitle("About Us");
+        activity.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setHasOptionsMenu(true);
+
+        DrawerLayout drawer = (DrawerLayout) activity.findViewById(R.id.drawer);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                getActivity(), drawer, toolbar, R.string.drawer_open, R.string.drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
        return mView;
     }
 }
--- app/src/main/java/com/geon/lbs/ui/fragment/ProfileFragment.java
+++ app/src/main/java/com/geon/lbs/ui/fragment/ProfileFragment.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,7 +19,7 @@ import com.geon.lbs.R;
 public class ProfileFragment extends Fragment {
 
     View mView;
-    TextView userName,userCategory,userPhone,userEmail;
+    TextView userName,userPhone,userEmail;
     AppGlobal appGlobal;
 
     @Nullable
@@ -25,20 +29,29 @@ public class ProfileFragment extends Fragment {
         mView = inflater.inflate(R.layout.fragment_profile,container,false);
         appGlobal =((AppGlobal)getActivity().getApplicationContext());
         bindView();
+        Toolbar toolbar = (Toolbar)mView.findViewById(R.id.app_bar);
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        activity.setSupportActionBar(toolbar);
+        activity.getSupportActionBar().setTitle("Profile");
+        activity.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setHasOptionsMenu(true);
+
+        DrawerLayout drawer = (DrawerLayout) activity.findViewById(R.id.drawer);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                getActivity(), drawer, toolbar, R.string.drawer_open, R.string.drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
         return mView;
     }
     void bindView(){
         userName = mView.findViewById(R.id.user_name);
-        userCategory = mView.findViewById(R.id.user_category);
         userPhone = mView.findViewById(R.id.user_phone);
         userEmail = mView.findViewById(R.id.user_email);
 
         String userNameStr = appGlobal.userFirsName + " "+appGlobal.userLastName;
-        String userCatStr = "Category : "+appGlobal.userCatagory;
-        String userPhoneStr = "Phone : "+ appGlobal.userPhone;
-        String userEmailStr = "Email : " + appGlobal.userEmail;
+        String userPhoneStr = appGlobal.userPhone;
+        String userEmailStr = appGlobal.userEmail;
         userName.setText(userNameStr);
-        userCategory.setText(userCatStr);
         userPhone.setText(userPhoneStr);
         userEmail.setText(userEmailStr);
     }
